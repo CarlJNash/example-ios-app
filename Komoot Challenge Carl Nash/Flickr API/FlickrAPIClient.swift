c7b5ef35ea0710af2a48cd4e08f99a340cb3fd63
Carl Nash <c.nash@waracle.com>
2021-04-26 12:11:56 +0100

    Update header docs

--- Komoot Challenge Carl Nash/Flickr API/FlickrAPIClient.swift
+++ Komoot Challenge Carl Nash/Flickr API/FlickrAPIClient.swift
@@ -9,7 +9,25 @@ import Foundation
 import UIKit
 
 protocol APIClient {
+    /// Method for for searching for photos from the Flickr Photo Search API based on GPS coordinates.
+    /// This response contains information such as the `imageId`, `server` and `secret` that are used for downloading the image.
+    ///
+    /// - Parameters:
+    ///   - lat: The GPS latitude value.
+    ///   - lon: The GPS longitude value.
+    ///   - completion: `Result` object containing a `APIPhotosSearchResponse` if successful, otherwise an `Error` on failure.
+    ///
+    /// - SeeAlso: https://www.flickr.com/services/api/flickr.photos.search.html
     func searchForPhotosForLocation(lat: Double, lon: Double, completion: @escaping (Result<APIPhotosSearchResponse, Error>) -> Void)
+    
+    /// Method for downloading an image from Flickr based on the  values from the photos search response. `APIPhotosSearchResponse`.
+    ///
+    /// - Parameters:
+    ///   - serverId: The Flickr server ID
+    ///   - id: The ID of the photo
+    ///   - secret: The secret of the photo
+    ///   - photoSize: The photo size required, see `FlickrPhotoSize`
+    ///   - completion: `Result` object containing a `UIImage` if successful, otherwise an `Error` on failure
     func downloadPhoto(serverId: String, id: String, secret: String, photoSize: FlickrPhotoSize, completion: @escaping (Result<UIImage, Error>) -> Void)
 }
 
@@ -26,11 +44,6 @@ struct FlickrAPIClient: APIClient {
         urlSession = URLSession(configuration: .default)
     }
     
-    /// Use this for searching for photos from the Flickr API based on GRP location
-    /// - Parameters:
-    ///   - lat: The GPS latitude value
-    ///   - lon: The GPS longitude value
-    ///   - completion: `Result` object containing a `APIPhotosSearchResponse` if successful, otherwise an `Error` on failure
     func searchForPhotosForLocation(lat: Double, lon: Double, completion: @escaping (Result<APIPhotosSearchResponse, Error>) -> Void) {
         let queryItems: [URLQueryItem] = [
             .init(name: "method", value: "flickr.photos.search"),
@@ -38,6 +51,11 @@ struct FlickrAPIClient: APIClient {
             .init(name: "lat", value: String(lat)),
             .init(name: "lon", value: String(lon)),
             .init(name: "radius", value: "10"),
+            .init(name: "safe_search", value: "1"), // safe
+            .init(name: "content_type", value: "1"), // photos only
+            .init(name: "privacy_filter", value: "1"), // public photos
+            .init(name: "tags", value: "landscape, komoot"), // Try and get more relevant photos
+            .init(name: "geo_context", value: "2"), // outdoors
             .init(name: "format", value: "json"),
             .init(name: "nojsoncallback", value: "1")
         ]
@@ -65,13 +83,6 @@ struct FlickrAPIClient: APIClient {
         }.resume()
     }
     
-    /// For downloading an image from Flickr based on the  values from the photos search response `APIPhotosSearchResponse`
-    /// - Parameters:
-    ///   - serverId: The Flickr server ID
-    ///   - id: The ID of the photo
-    ///   - secret: The secret of the photo
-    ///   - photoSize: The photo size required, see `FlickrPhotoSize`
-    ///   - completion: `Result` object containing a `UIImage` if successful, otherwise an `Error` on failure
     func downloadPhoto(serverId: String, id: String, secret: String, photoSize: FlickrPhotoSize, completion: @escaping (Result<UIImage, Error>) -> Void) {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
